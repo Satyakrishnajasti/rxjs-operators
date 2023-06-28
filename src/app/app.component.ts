7fbc89b0050e7306b72190518c13cb4f052d12cc
Jasti Krishna <jkrishna@ariqt.com>
2023-06-28 11:57:28 +0530

    changed the code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { Subject, concatMap, first, forkJoin, map, mergeMap, reduce, switchMap, take, filter, takeWhile, every, find, findIndex, isEmpty, max, min, count, tap, delay, partition, last, takeLast } from 'rxjs';
+import { Subject, concatMap, first, forkJoin, map, mergeMap, reduce, switchMap, take, filter, takeWhile, every, find, findIndex, isEmpty, max, min, count, tap, delay, partition, last, takeLast, startWith, endWith, Observable } from 'rxjs';
 import { SharedService } from './shared.service';
 
 export interface Model {
@@ -29,65 +29,74 @@ export class AppComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     // Map
-    this.shared.getPostAlbums().pipe(switchMap((data: any) => data), map((data: any) => {
+    let mapOp = this.shared.getPostAlbums().pipe(switchMap((data: any) => data), map((data: any) => {
       return {
         userId: data.userId,
         id: data.id * 10
       }
-    })).subscribe((data) => console.log(data));
+    }))
     //Filter
-    this.shared.getPostAlbums().pipe(mergeMap((data: any) => data), filter((data: any) => data.userId == 2), findIndex((data: any) => data.userId == 2)).subscribe((data) => console.log(`Filter ` + data));
-
+    let filterOpe: Observable<any> = this.shared.getPostAlbums().pipe(mergeMap((data: any) => data), filter((data: any) => data.userId == 2), findIndex((data: any) => data.userId == 2));
     // IsEmpty
-    this.shared.getPostAlbums().pipe(isEmpty()).subscribe((data) => console.log(`isEmpty ` + data));
+    let isEmptyOpera: Observable<any> = this.shared.getPostAlbums().pipe(isEmpty());
 
     // Max
-    this.shared.getPostAlbums().pipe(switchMap((data: any) => data), max((a: any, b: any) => a.id < b.id ? -1 : 1)).subscribe((data) => console.log(data));
+    let maximum: Observable<any> = this.shared.getPostAlbums().pipe(switchMap((data: any) => data), max((a: any, b: any) => a.id < b.id ? -1 : 1))
     // Reduce
-    this.shared.getPostAlbums().pipe(switchMap((data: any) => data), reduce((a: any, b: any) => Number(a.id) + Number(b.id))).subscribe((data) => console.log(`reduce ` + data));
+    let reduceOp: Observable<any> = this.shared.getPostAlbums().pipe(switchMap((data: any) => data), reduce((a: any, b: any) => Number(a.id) + Number(b.id)))
 
     // Min
-    this.shared.getPostAlbums().pipe(switchMap((data: any) => data), min((a: any, b: any) => a.id < b.id ? -1 : 1)).subscribe((data) => console.log(data));
+    let minimim = this.shared.getPostAlbums().pipe(switchMap((data: any) => data), min((a: any, b: any) => a.id < b.id ? -1 : 1))
 
     // Count
-    this.shared.getPostAlbums().pipe(switchMap((data: any) => data), count((data: any) => data.userId % 5 == 0)).subscribe((data) => console.log(data));
+    let countOp: Observable<any> = this.shared.getPostAlbums().pipe(switchMap((data: any) => data), count((data: any) => data.userId % 5 == 0))
 
     // Tap
-    this.shared.getPostAlbums().pipe(delay(2000), tap((data: any) => data)).subscribe((data) => console.log(data));
+    let tapOp: Observable<any> = this.shared.getPostAlbums().pipe(delay(2000), tap((data: any) => data))
 
     // ForkJoin
     const posts = this.http.get(`https://jsonplaceholder.typicode.com/posts`);
     const albums = this.http.get(`https://jsonplaceholder.typicode.com/albums`);
     const album = this.http.get(`https://jsonplaceholder.typicode.com/albums`);
-    forkJoin([posts, albums, album]).pipe(take(1)).subscribe((data) => console.log(data));
+    let fork = forkJoin([posts, albums, album]).pipe(take(1));
 
     // Mergemap
-    this.shared.getPostAlbums().pipe(switchMap((data: any) => data), filter((data: any) => data.id == 10), mergeMap((data) => this.http.get(`https://jsonplaceholder.typicode.com/posts?userId=${data.userId}`))).subscribe((data) => console.log(data));
+    let mergeMapOp = this.shared.getPostAlbums().pipe(switchMap((data: any) => data), filter((data: any) => data.id == 10), mergeMap((data) => this.http.get(`https://jsonplaceholder.typicode.com/posts?userId=${data.userId}`)));
+
 
     // SwitchMap
-    this.shared.getPostAlbums().pipe(switchMap((element1) => {
+    let switchOp = this.shared.getPostAlbums().pipe(switchMap((element1) => {
       return this.shared.getPostsData().pipe(map((element2) => ({ element1, element2 })))
-    })).subscribe((data) => console.log(data));
+    }))
 
     // First
-    this.shared.getPostAlbums().pipe(switchMap((data: any) => data), first()).subscribe((data) => console.log(data));
+    let firstOp = this.shared.getPostAlbums().pipe(switchMap((data: any) => data), first());
 
     // Last
-    this.shared.getPostAlbums().pipe(switchMap((data: any) => data), last()).subscribe((data) => console.log(data));
+    let lastOp = this.shared.getPostAlbums().pipe(switchMap((data: any) => data), last())
 
     // Take
-    this.shared.getPostAlbums().pipe(mergeMap((data: any) => data), take(10)).subscribe((data) => console.log(data));
+    let takeOp = this.shared.getPostAlbums().pipe(mergeMap((data: any) => data), take(10))
 
     // TakeLast
-    this.shared.getPostAlbums().pipe(mergeMap((data: any) => data), takeLast(10)).subscribe((data) => console.log(data));
+    let takeLastOp = this.shared.getPostAlbums().pipe(mergeMap((data: any) => data), takeLast(10))
 
     // TakeWhile
-    this.shared.getPostAlbums().pipe(mergeMap((data: any) => data), filter((data: any) => data.userId == 5), takeWhile((data: any) => data.id == 41)).subscribe((data) => console.log(data));
+    let takeWhileOp = this.shared.getPostAlbums().pipe(mergeMap((data: any) => data), filter((data: any) => data.userId == 5), takeWhile((data: any) => data.id == 41))
 
     // startWith
-    
-    // endWith
+    let start = this.shared.getPostAlbums().pipe(map((data: any) => data), startWith('Welcome'))
 
+    // endWith
+    let end = this.shared.getPostAlbums().pipe(map((data: any) => data), endWith('Welcome'));
+
+    // Find
+    let findOp = this.shared.getPostAlbums().pipe(mergeMap((data: any) => data), find((data: any) => data.userId == 10));
+
+    // FindIndex
+    let findIndexOp=this.shared.getPostAlbums().pipe(mergeMap((data: any) => data), findIndex((data: any) => data.userId == 10));
+
+    findIndexOp.subscribe((data) => console.log(data));
 
   }
 
