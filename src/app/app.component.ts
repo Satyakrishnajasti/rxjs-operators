3b07c484134bc0c3e65766e79dd34b5d750d8f21
Satytakrishnajasti <satyajasti9@gmail.com>
2023-07-04 22:15:08 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,47 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { Subject, concatMap, first, forkJoin, map, mergeMap, reduce, switchMap, take, filter, takeWhile, every, find, findIndex, isEmpty, max, min, count, tap, delay, partition, last, takeLast, startWith, endWith, Observable, distinct, from, range, of, interval, combineLatest } from 'rxjs';
+import {
+  Subject,
+  concatMap,
+  first,
+  forkJoin,
+  map,
+  mergeMap,
+  reduce,
+  switchMap,
+  take,
+  filter,
+  takeWhile,
+  every,
+  find,
+  findIndex,
+  isEmpty,
+  max,
+  min,
+  count,
+  tap,
+  delay,
+  partition,
+  last,
+  takeLast,
+  startWith,
+  endWith,
+  Observable,
+  distinct,
+  from,
+  range,
+  of,
+  interval,
+  combineLatest,
+  concat,
+} from 'rxjs';
 import { SharedService } from './shared.service';
 
 export interface Model {
-  "id": number;
-  "userId": number;
-  "title": string;
+  id: number;
+  userId: number;
+  title: string;
 }
 
 export interface Operators {
@@ -20,7 +54,7 @@ export interface Operators {
   imports: [CommonModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
-  providers: [SharedService]
+  providers: [SharedService],
 })
 export class AppComponent implements OnInit, OnDestroy {
   title = 'rxjs-operators';
@@ -30,114 +64,143 @@ export class AppComponent implements OnInit, OnDestroy {
 
   protected Operators: Operators[] = [
     {
-      name: 'Filter'
+      name: 'Filter',
     },
     {
-      name: 'IsEmpty'
+      name: 'IsEmpty',
     },
     {
-      name: 'Max'
+      name: 'Max',
     },
     {
-      name: 'Min'
+      name: 'Min',
     },
     {
-      name: 'Reduce'
+      name: 'Reduce',
     },
     {
-      name: 'Count'
+      name: 'Count',
     },
     {
-      name: 'Tap'
+      name: 'Tap',
     },
     {
-      name: 'ForkJoin'
+      name: 'ForkJoin',
     },
     {
-      name: 'MergeMap'
+      name: 'MergeMap',
     },
     {
-      name: 'SwitchMap'
+      name: 'SwitchMap',
     },
     {
-      name: 'First'
+      name: 'First',
     },
     {
-      name: 'Last'
+      name: 'Last',
     },
     {
-      name: 'Take'
+      name: 'Take',
     },
     {
-      name: 'Take Last'
+      name: 'Take Last',
     },
     {
-      name: 'Take While'
+      name: 'Take While',
     },
     {
-      name: 'Startwith'
+      name: 'Startwith',
     },
     {
-      name: 'Endwith'
+      name: 'Endwith',
     },
     {
-      name: 'Find'
+      name: 'Find',
     },
     {
-      name: 'FindIndex'
+      name: 'FindIndex',
     },
     {
-      name: 'Distinct'
+      name: 'Distinct',
     },
     {
-      name: 'From'
+      name: 'From',
     },
     {
-      name: 'Range'
+      name: 'Range',
     },
     {
-      name: 'Interval'
+      name: 'Interval',
     },
     {
-      name: 'Of'
+      name: 'Of',
     },
     {
-      name: 'CombineLatest'
+      name: 'CombineLatest',
     },
     {
-      name: 'Delay'
-    }
+      name: 'Delay',
+    },
+    {
+      name: 'Concat',
+    },
   ];
 
-  constructor(private readonly shared: SharedService, private readonly http: HttpClient) {
-  }
+  constructor(
+    private readonly shared: SharedService,
+    private readonly http: HttpClient
+  ) {}
 
   ngOnInit(): void {
     // Map
-    let mapOp = this.shared.getPostAlbums().pipe(switchMap((data: any) => data), map((data: any) => {
-      return {
-        userId: data.userId,
-        id: data.id * 10
-      }
-    }))
+    let mapOp = this.shared.getPostAlbums().pipe(
+      switchMap((data: any) => data),
+      map((data: any) => {
+        return {
+          userId: data.userId,
+          id: data.id * 10,
+        };
+      })
+    );
     //Filter
-    let filterOpe: Observable<any> = this.shared.getPostAlbums().pipe(mergeMap((data: any) => data), filter((data: any) => data.userId == 2), findIndex((data: any) => data.userId == 2));
+    let filterOpe: Observable<any> = this.shared.getPostAlbums().pipe(
+      mergeMap((data: any) => data),
+      filter((data: any) => data.userId == 2),
+      findIndex((data: any) => data.userId == 2)
+    );
     // IsEmpty
-    let isEmptyOpera: Observable<any> = this.shared.getPostAlbums().pipe(isEmpty());
+    let isEmptyOpera: Observable<any> = this.shared
+      .getPostAlbums()
+      .pipe(isEmpty());
 
     // Max
-    let maximum: Observable<any> = this.shared.getPostAlbums().pipe(switchMap((data: any) => data), max((a: any, b: any) => a.id < b.id ? -1 : 1))
+    let maximum: Observable<any> = this.shared.getPostAlbums().pipe(
+      switchMap((data: any) => data),
+      max((a: any, b: any) => (a.id < b.id ? -1 : 1))
+    );
     // Reduce
-    let reduceOp: Observable<any> = this.shared.getPostAlbums().pipe(switchMap((data: any) => data), reduce((a: any, b: any) => Number(a.id) + Number(b.id)))
+    let reduceOp: Observable<any> = this.shared.getPostAlbums().pipe(
+      switchMap((data: any) => data),
+      reduce((a: any, b: any) => Number(a.id) + Number(b.id))
+    );
 
     // Min
-    let minimim = this.shared.getPostAlbums().pipe(switchMap((data: any) => data), min((a: any, b: any) => a.id < b.id ? -1 : 1))
+    let minimim = this.shared.getPostAlbums().pipe(
+      switchMap((data: any) => data),
+      min((a: any, b: any) => (a.id < b.id ? -1 : 1))
+    );
 
     // Count
-    let countOp: Observable<any> = this.shared.getPostAlbums().pipe(switchMap((data: any) => data), count((data: any) => data.userId % 5 == 0))
+    let countOp: Observable<any> = this.shared.getPostAlbums().pipe(
+      switchMap((data: any) => data),
+      count((data: any) => data.userId % 5 == 0)
+    );
 
     // Tap
-    let tapOp: Observable<any> = this.shared.getPostAlbums().pipe(delay(2000), tap((data: any) => data))
+    let tapOp: Observable<any> = this.shared.getPostAlbums().pipe(
+      delay(2000),
+      tap((data: any) => data)
+    );
 
     // ForkJoin
     const posts = this.http.get(`https://jsonplaceholder.typicode.com/posts`);
@@ -146,43 +209,85 @@ export class AppComponent implements OnInit, OnDestroy {
     let fork = forkJoin([posts, albums, album]).pipe(take(1));
 
     // Mergemap
-    let mergeMapOp = this.shared.getPostAlbums().pipe(switchMap((data: any) => data), filter((data: any) => data.id == 10), mergeMap((data) => this.http.get(`https://jsonplaceholder.typicode.com/posts?userId=${data.userId}`)));
-
+    let mergeMapOp = this.shared.getPostAlbums().pipe(
+      switchMap((data: any) => data),
+      filter((data: any) => data.id == 10),
+      mergeMap((data) =>
+        this.http.get(
+          `https://jsonplaceholder.typicode.com/posts?userId=${data.userId}`
+        )
+      )
+    );
 
     // SwitchMap
-    let switchOp = this.shared.getPostAlbums().pipe(switchMap((element1) => {
-      return this.shared.getPostsData().pipe(map((element2) => ({ element1, element2 })))
-    }))
+    let switchOp = this.shared.getPostAlbums().pipe(
+      switchMap((element1) => {
+        return this.shared
+          .getPostsData()
+          .pipe(map((element2) => ({ element1, element2 })));
+      })
+    );
 
     // First
-    let firstOp = this.shared.getPostAlbums().pipe(switchMap((data: any) => data), first());
+    let firstOp = this.shared.getPostAlbums().pipe(
+      switchMap((data: any) => data),
+      first()
+    );
 
     // Last
-    let lastOp = this.shared.getPostAlbums().pipe(switchMap((data: any) => data), last())
+    let lastOp = this.shared.getPostAlbums().pipe(
+      switchMap((data: any) => data),
+      last()
+    );
 
     // Take
-    let takeOp = this.shared.getPostAlbums().pipe(mergeMap((data: any) => data), take(10))
+    let takeOp = this.shared.getPostAlbums().pipe(
+      mergeMap((data: any) => data),
+      take(10)
+    );
 
     // TakeLast
-    let takeLastOp = this.shared.getPostAlbums().pipe(mergeMap((data: any) => data), takeLast(10))
+    let takeLastOp = this.shared.getPostAlbums().pipe(
+      mergeMap((data: any) => data),
+      takeLast(10)
+    );
 
     // TakeWhile
-    let takeWhileOp = this.shared.getPostAlbums().pipe(mergeMap((data: any) => data), filter((data: any) => data.userId == 5), takeWhile((data: any) => data.id == 41))
+    let takeWhileOp = this.shared.getPostAlbums().pipe(
+      mergeMap((data: any) => data),
+      filter((data: any) => data.userId == 5),
+      takeWhile((data: any) => data.id == 41)
+    );
 
     // startWith
-    let start = this.shared.getPostAlbums().pipe(map((data: any) => data), startWith('Welcome'))
+    let start = this.shared.getPostAlbums().pipe(
+      map((data: any) => data),
+      startWith('Welcome')
+    );
 
     // endWith
-    let end = this.shared.getPostAlbums().pipe(map((data: any) => data), endWith('Welcome'));
+    let end = this.shared.getPostAlbums().pipe(
+      map((data: any) => data),
+      endWith('Welcome')
+    );
 
     // Find
-    let findOp = this.shared.getPostAlbums().pipe(mergeMap((data: any) => data), find((data: any) => data.userId == 10));
+    let findOp = this.shared.getPostAlbums().pipe(
+      mergeMap((data: any) => data),
+      find((data: any) => data.userId == 10)
+    );
 
     // FindIndex
-    let findIndexOp = this.shared.getPostAlbums().pipe(mergeMap((data: any) => data), findIndex((data: any) => data.userId == 10));
+    let findIndexOp = this.shared.getPostAlbums().pipe(
+      mergeMap((data: any) => data),
+      findIndex((data: any) => data.userId == 10)
+    );
 
     // Distinct
-    let distinctOp = this.shared.getPostAlbums().pipe(mergeMap((data: any) => data), distinct((data: any) => data.userId));
+    let distinctOp = this.shared.getPostAlbums().pipe(
+      mergeMap((data: any) => data),
+      distinct((data: any) => data.userId)
+    );
 
     //From
     let fruits = from([{ name: 'Apple' }, { name: 'Mango' }]);
@@ -200,12 +305,14 @@ export class AppComponent implements OnInit, OnDestroy {
     let combine = combineLatest([album, posts, albums]);
 
     // delay
-    let d = range(1, 10).pipe(delay(5000))
+    let d = range(1, 10).pipe(delay(5000));
 
+    // concat
+    let post = this.shared.getPostAlbums();
+    let albumss = this.shared.getPostsData();
+    let result = concat(post, albumss);
 
-
-    d.subscribe((data) => console.log(data));
-
+    result.subscribe((data) => console.log(data));
   }
 
   ngOnDestroy(): void {
